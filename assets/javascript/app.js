d68bbd90ecd2b70c29ab3a7efc4e0c465a064649
cesar avina <cesaravina@gmail.com>
2017-03-04 17:50:25 -0800

    ready to publish

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,77 +7,80 @@ $(document).ready(function() {
         correctAnswer = 0,
         incorrectAnswer = 0,
         blank = 0,
-        count = 20;
+        count = 30;
 
     // for loop + if/else to match userGuess
     // true or false matches
+//for loop and if else statement to see how many user selectionsa and answers match
 
-        function scoreCount() {
-            for (var i = 0; i < data.length; i++) {
-            // if user selects an answer
-            if(data[i].checked){
+    function scoreCount() {
+        for (var i = 0; i < data.length; i++) {
 
-                // Check if user selcetion = array of answers, ++
-                if(answers.indexOf(data[i].value) !== -1) {
+            // If user selected an answer
+            if (data[i].checked) {
+
+                // check if what the user select is equal to the array answers
+
+                if (answers.indexOf(data[i].value) !== -1) {
                     correctAnswer++;
                 } else {
                     incorrectAnswer++;
                 }
             }
         }
-
-    // check unanswered questions by subtracting if/else from above from the total number of questions
-
-    var totalAnswered = correctAnswer + incorrectAnswer;
-    console.log(totalAnswered);
-    if(totalAnswered !== totalQuiz){
-        blank = totalQuiz - totalAnswered;
-}
-    
-
-    $("#correct").html(" " + correctAnswer);
-    $("#incorrect").html(" " + incorrectAnswer);
-    $("#blank").html(" " + blank);
-}// end scoreCount
-
-// show/hide events
-// hide until click.play
-$("#quiz, #results").hide();
-
-// show questions & start timer
-$("#play").click(function() {
-    $("#start").hide("slow");
-    $("#quiz").show("slow");
-// timer:
-
-
-var startTimer = setInterval (function(){
-    count--;
-    $("#countdown").html(count);
-
-    // alert and show questions when time's up
-
-    if(count === 0){
-        clearInterval(timer);
-        $("#quiz, #timer").hide("slow");
-        $("results").show("slow");
-        scoreCount();
+        //check how many questions were blank by subtracting the if/else values from above from the total number of questions.
+        
+        var totalAnswered = correctAnswer + incorrectAnswer;
+        console.log(totalAnswered);
+        if (totalAnswered !== totalQuiz) {
+            blank = totalQuiz - totalAnswered;
         }
-    }, 2000);
-});
- 
-// Bottoms up (done) button brings up results page
 
-$("#done").click(function(){
-    $("#quiz, #timer").hide("slow");
-    $("#results").show("slow");
-    clearInterval(timer);
-    scoreCount();
-})
+        $('#correct').html(" " + correctAnswer);
+        $('#incorrect').html(" " + incorrectAnswer);
+        $("#blank").html(" " + blank);
 
-$("restart").click(function(){
-    location.reload();
+    } //end scoreCount
+
+    //Hide and Show events
+    //hide quiz until click play\\
+    $("#quiz, #results").hide();
+
+    //questions show and timer begins\\
+    $("#play").click(function() {
+        $("#start").hide("slow");
+        $("#quiz").show("slow");
+
+        //Setup timer to countdown from 60 seconds total to answer all questions\\
+
+        var startTimer = setInterval(function() {
+            count--;
+            $("#countdown").html(count);
+
+            //if user runs out of time before completing questions, alert fail, show correct answers and calculate results.\\
+
+            if (count === 0) {
+                clearInterval(timer);
+                $("#quiz, #timer").hide("slow");
+                $("#results").show("slow");
+                scoreCount();
+            }
+        }, 1000);
+
     });
 
-});
+    //done button sends to results page//
 
+    $("#done").click(function() {
+        $("#quiz, #timer").hide("slow");
+        $("#results").show("slow");
+        clearInterval(timer);
+        scoreCount();
+    });
+
+    //restart button refreshes page back to start screen//
+
+    $("#restart").click(function() {
+        location.reload();
+    });
+});
